1d8e94295a5b07d6f093c252db23de25dda557ee
Fuyuu <dndkgds@gmail.com>
2024-11-18 14:17:58 +0900

    creating project

---
+++ apihw/src/main/java/edu/hongik/apihw/ApihwApplication.java
@@ -0,0 +1,13 @@
+package edu.hongik.apihw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApihwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApihwApplication.class, args);
+	}
+
+}
---
+++ apihw/src/test/java/edu/hongik/apihw/ApihwApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hongik.apihw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApihwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
